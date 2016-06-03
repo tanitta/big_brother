5f178d5653e4a28be9448d274a393e420b6e78c6
tanitta <ttata.trit@gmail.com>
2016-06-03 19:35:36 +0900

    Store User class and other method to Gatherer module.

--- bb_gatherer/lib/user.rb
+++ bb_gatherer/lib/user.rb
@@ -1,37 +1,40 @@
 require 'uri'
 require 'net/https'
 require 'nokogiri'
+module Gatherer
+  class User
+    def initialize(user_name, options = {search_limit:10})
+      @name = user_name
+      scrape_user(options[:search_limit])
+    end
 
-class User
-  def initialize(user_name, options = {search_limit:10})
-    @name = user_name
-    scrape_user(options[:search_limit])
-  end
-  
-  attr_reader :name, :relations
-  
-  private
-  def scrape_user(search_limit)
-    html = html_from_uri("http://twilog.org/#{ name }/friends")
-    
-    if exist_user?
-      pages = 1
+    attr_reader :name, :relations
+
+    private
+    def scrape_user(search_limit)
+      html = Gatherer::html_from_uri("http://twilog.org/#{ @name }/friends")
+      
+      exist_user = false
+      if exist_user
+        pages = 1
+      end
+    end
+
+    def scrape_user_page(search_limit, page_number)
+      html = Gatherer::html_from_uri("http://twilog.org/#{ @name }/friends/r-#{ page_number }")
     end
   end
-  
-  def scrape_user_page(search_limit, page_number)
-    html = html_from_uri("http://twilog.org/#{ name }/friends/r-#{ page_number }")
-  end
-end
 
-def html_from_uri(uri_string)
-  uri = URI.parse(uri_string)
-  http = Net::HTTP.new(uri.host, uri.port)
-  if uri.scheme == 'https'
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  def html_from_uri(uri_string)
+    uri = URI.parse(uri_string)
+    http = Net::HTTP.new(uri.host, uri.port)
+    if uri.scheme == 'https'
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    end
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    return Nokogiri::HTML(response.body)
   end
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-  return Nokogiri::HTML(response.body)
+  module_function :html_from_uri
 end
